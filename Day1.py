942d14b479abfe69ca96ec2f3bd312d3c8e4c698
abhinav250401 <58158362+abhinav250401@users.noreply.github.com>
2023-06-07 20:57:19 +0530

    Created Day1.py

---
+++ Day1.py
@@ -0,0 +1,9 @@
+#Day 1 Project
+#Title: Band Name Generator
+#Aim: To learn the basics of Python Syntax and print functions.
+print("Welcome to Band Name Generator\n")
+#Taking Place of Birth as input from the user.
+city=input("What is the name of the place where you were born?\n")
+#Taking the pet's name as input
+pet=input("What is the name of your pet?\n")
+print("A possible name for your band could be "+city+" "+pet+"\n")
